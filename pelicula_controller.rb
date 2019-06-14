5a0d0293d925ae21ee3a3c19ed4274847498fb02
Vicentevialpaul <30669451+Vicentevialpaul@users.noreply.github.com>
2019-06-14 15:18:02 -0400

    Add files via upload

---
+++ pelicula_controller.rb
@@ -0,0 +1,31 @@
+require "rubygems"
+require "httparty"
+
+class PeliculaController < ApplicationController
+  include HTTParty
+  def index
+    self.params = params.permit!
+    @urls =  params[:informacion]
+    idp = @urls["id"]
+    response1 =  HTTParty.post("https://swapi-graphql-integracion-t3.herokuapp.com/",
+               headers: {'content-type': 'application/json'},
+               body:{"query": "query{film(id: \"#{idp}\"){title,releaseDate,episodeID,openingCrawl,director,producers,
+                       characterConnection{characters{name,id}},
+                       planetConnection{planets{name,id}},
+                       starshipConnection{starships{name,id}},
+                       }}"}.to_json)
+    @response = JSON.parse(response1.body)
+	@urls = @response["data"]["film"]
+	@title = @urls["title"]
+	@episode = @urls["episodeID"]
+	@description = @urls["openingCrawl"]
+	@director = @urls["director"]
+	@producer = @urls["producers"]
+	@año = @urls["releaseDate"]
+    @characters = @urls["characterConnection"]["characters"]
+    @planets = @urls["planetConnection"]["planets"]
+    @starships = @urls["starshipConnection"]["starships"]
+  end
+end
+
+
